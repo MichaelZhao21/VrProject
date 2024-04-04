318a04c815574aeb4eff5109ed78181c19740e13
Salman Jaher <salmanjaher1@gmail.com>
2024-04-03 22:04:24 -0500

    burner + prefab

--- Assets/Scripts/Burner.cs
+++ Assets/Scripts/Burner.cs
@@ -6,16 +6,31 @@ public class Burner : MonoBehaviour
 {
 
     [SerializeField] public GameObject burner;
+    private bool isBurning = false;
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void BurnerOn(){
+        // Change color of burner to red
+        burner.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void BurnerOff(){
+        // Change color of burner to black
+        burner.GetComponent<Renderer>().material.color = Color.black;
     }
 
     // Update is called once per frame
-    void Update()
+    public void CallScript()
     {
-        
+        if (isBurning == false)
+        {
+            BurnerOn();
+            isBurning = true;
+        }
+        else
+        {
+            BurnerOff();
+            isBurning = false;
+        }
     }
 }
