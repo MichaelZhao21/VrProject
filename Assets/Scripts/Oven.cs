6e83cc2f825f026c029367934caa1fdf31ad494c
Salman Jaher <salmanjaher1@gmail.com>
2024-04-08 18:02:14 -0500

    changed oven cooking script

---
+++ Assets/Scripts/Oven.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven : MonoBehaviour
+{
+    private bool isBaking = false;
+
+    private Color defaultColor;
+
+    private bool coRoutine = false;
+
+    private List<GameObject> insideOven = new List<GameObject>();
+    
+    void Start(){
+        defaultColor = gameObject.GetComponent<Renderer>().materials[1].color;
+    }
+    void OvenOn(){
+        // Change color of oven to red
+        gameObject.GetComponent<Renderer>().materials[1].color = Color.yellow;
+    }
+
+    void OvenOff(){
+        // Change color of oven to black
+        gameObject.GetComponent<Renderer>().materials[1].color = defaultColor;
+    }
+
+    // Update is called once per frame
+    public void CallScript()
+    {
+        if (isBaking == false)
+        {
+            OvenOn();
+            isBaking = true;
+        }
+        else
+        {
+            OvenOff();
+            isBaking = false;
+        }
+    }
+
+    void Update(){
+        if (isBaking){
+            foreach (GameObject food in insideOven){
+                if (food.tag == "ingredient"){
+                    if (!coRoutine)
+                    {
+                        StartCoroutine(cookFood(food));
+                    }
+                }
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other){
+        insideOven.Add(other.gameObject);
+    }
+
+    public void OnTriggerExit(Collider other){
+        other.gameObject.GetComponent<Ingredient>().isCooking = false;
+        insideOven.Remove(other.gameObject);
+    }
+
+
+    IEnumerator cookFood(GameObject food)
+    {
+        coRoutine = true;
+        yield return new WaitForSeconds(2);
+        // If the object is not already cooking, start cooking it
+        if (food.GetComponent<Ingredient>().CookingPercentage < 120)
+        {
+            food.GetComponent<Ingredient>().isCooking = true;
+            food.GetComponent<Ingredient>().CookingPercentage += 10;
+            if (food.GetComponent<Ingredient>().CookingPercentage == 100)
+            {
+                food.GetComponent<Renderer>().material.color = Color.black;
+                food.GetComponent<Ingredient>().isCooked = true;
+                gameObject.GetComponent<AudioSource>().Play();
+
+            }
+            if (food.GetComponent<Ingredient>().CookingPercentage >= 120)
+            {
+                food.GetComponent<Renderer>().material.color = Color.red;
+                gameObject.GetComponent<AudioSource>().Play();
+                food.GetComponent<Ingredient>().isCooking = false;
+            }
+        }
+
+        coRoutine = false;
+    }
+
+}
