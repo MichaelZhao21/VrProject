b472915d9c785155a1ee8bb0d6caa942cbf84cf9
Azlan Jaher <azlanjaher@gmail.com>
2024-04-29 17:18:41 -0500

    Most of the tutorial
    Co-Authored-By: Matthew Tate <89745826+matthewjtate79@users.noreply.github.com>

---
+++ Assets/NetworkPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform cook;
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.IsMine) 
+        {
+            cook.gameObject.SetActive(false);
+
+            MapPosition(cook, XRNode.Head);
+        }
+        
+    }
+
+    void MapPosition(Transform target, XRNode node)
+    {
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+        target.position = position;
+        target.rotation = rotation;
+    }
+}
