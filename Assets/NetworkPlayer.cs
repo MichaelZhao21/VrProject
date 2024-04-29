636ce31404c03038045649d0eb4a765452378049
Azlan Jaher <azlanjaher@gmail.com>
2024-04-29 18:45:12 -0500

    Chicky
    testing server view obj
    
    Co-Authored-By: Matthew Tate <89745826+matthewjtate79@users.noreply.github.com>

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -24,23 +24,13 @@ public class NetworkPlayer : MonoBehaviour
         {
             cook.gameObject.SetActive(false);
 
-            // MapPosition(cook, XRNode.Head);
-
             cam = GameObject.Find("Main Camera");
             cook.transform.position = new Vector3(cam.transform.position.x, 0f, cam.transform.position.z);
             cook.transform.rotation = new Quaternion(0f, cam.transform.rotation.y, 0.0f, cam.transform.rotation.w);
-            //cook.transform.SetParent(cam.transform);
+            cook.transform.rotation *= Quaternion.Euler(0, 180, 0);
 
         }
         
     }
 
-    void MapPosition(Transform target, XRNode node)
-    {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
-        Debug.Log(position);
-        target.position = position;
-        target.rotation = rotation;
-    }
 }
