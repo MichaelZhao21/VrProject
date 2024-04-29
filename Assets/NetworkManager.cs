f92ef20cfb850d3eb11532b98b6601adf6d9ec98
Azlan Jaher <azlanjaher@gmail.com>
2024-04-29 16:14:56 -0500

    Basic Server
    added to MP test scene
    
    Co-Authored-By: Matthew Tate <89745826+matthewjtate79@users.noreply.github.com>

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Trying to connect");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default );
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room.");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("New player joined");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
