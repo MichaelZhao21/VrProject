6c82f80d436de85be477deeab9e60f895170522d
Salman Jaher <salmanjaher1@gmail.com>
2024-04-30 00:07:16 -0500

    stove mmmm

--- Assets/Scripts/Burner.cs
+++ Assets/Scripts/Burner.cs
@@ -9,14 +9,30 @@ public class Burner : MonoBehaviour
 
     private Color defaultColor;
 
+    private PhotonView pv;
+
     void Start()
     {
         // Save the default color of the burner
         defaultColor = GetComponent<MeshRenderer>().material.color;
+        pv = GetComponent<PhotonView>();
         
     }
 
     // Update is called once per frame
+    [PunRPC]
+    public void ToggleBurningRPC()
+    {
+        ToggleBurning();
+    }
+
+    public void click(){
+        // isBurning = !isBurning;
+        // GetComponent<Renderer>().material.color = isBurning ? Color.red : defaultColor;
+        pv.RPC("ToggleBurningRPC", RpcTarget.AllBuffered);
+    }
+
+
     public void ToggleBurning()
     {
         isBurning = !isBurning;
